b2ab183a533aade7ffec54b710026377b4c36754
craig <craig@amanzi.com>
2010-08-12 11:32:21 +0200

    initialize catalog with a search of likely database locations

--- src/net/refractions/udig/catalog/neo4j/Activator.java
+++ src/net/refractions/udig/catalog/neo4j/Activator.java
@@ -3,18 +3,26 @@
  */
 package net.refractions.udig.catalog.neo4j;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
+import net.refractions.udig.catalog.CatalogPlugin;
+import net.refractions.udig.catalog.ICatalog;
 import net.refractions.udig.catalog.IService;
+import net.refractions.udig.catalog.IServiceFactory;
 import net.refractions.udig.project.ILayer;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IStartup;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.geotools.data.DataStore;
 import org.neo4j.gis.spatial.geotools.data.Neo4jSpatialDataStore;
@@ -25,7 +33,7 @@ import org.osgi.framework.BundleContext;
 /**
  * @author Davide Savazzi
  */
-public class Activator extends AbstractUIPlugin {
+public class Activator extends AbstractUIPlugin implements IStartup {
 
 	// Constructor
 	    
@@ -37,8 +45,7 @@ public class Activator extends AbstractUIPlugin {
         dataStorefactory = new Neo4jSpatialDataStoreFactory();
         plugin = this;
     }
-    
-    
+
     // Public methods
     
 	public Neo4jSpatialDataStore getDataStore(Map<String, Serializable> params) throws IOException {
@@ -133,4 +140,68 @@ public class Activator extends AbstractUIPlugin {
     private static Activator plugin;
 
     public final static String ID = "net.refractions.udig.catalog.neo4j";
+
+	@Override
+    public void earlyStartup() {
+		if (this.openDataStores.size() == 0) {
+			ensureDefaultDatabasesLoaded();
+		}
+    }
+
+	private void ensureDefaultDatabasesLoaded() {
+        HashSet<File> dbDirs = new HashSet<File>();
+        for(String path: new String[]{".",System.getenv("HOME")}) {
+        	try {
+	        	File dir = new File(path);
+	        	if(dir.exists() && dir.isDirectory()) {
+		            for(String subdir: new String[]{"dev/neo4j","neo4j",".amanzi","workspace"}) {
+		            	findDbDirs(dbDirs, new File(dir.getCanonicalFile(),subdir), 0);
+		            }
+	        	}
+        	}catch(Exception e){
+        		System.err.println("Failed to perform search at '" + path + "': "+e);
+        	}
+        }
+        if(dbDirs.size()>0){
+    	    ICatalog catalog = CatalogPlugin.getDefault().getLocalCatalog();
+            IServiceFactory serviceFactory = CatalogPlugin.getDefault().getServiceFactory();
+	        for(File dir:dbDirs){
+	        	try {
+	                URL url = new File(dir,"neostore.id").toURI().toURL();
+	                System.out.println("Searching for service for "+url);
+	                for(IService service:serviceFactory.createService(url)){
+	                	System.out.println("Found service: "+service);
+                		catalog.add(service);
+	                }
+                } catch (MalformedURLException e) {
+                	System.err.println("Failed to find service for "+dir);
+	                e.printStackTrace();
+                }
+	        }
+        }
+    }
+    
+    private void findDbDirs(HashSet<File> dbDirs, File dir, int depth) {
+    	if(depth > 4) return;
+    	String[] files = dir.list();
+    	if(files.length > 50) return;
+       	for(String file:files){
+    		if(file.startsWith("neostore")){
+    			System.out.println("Found Neo4j Database at: "+dir);
+    			dbDirs.add(dir);
+    			return;
+    		}
+    	}
+       	for(String file:files){
+       		File f;
+            try {
+	            f = new File(dir.getCanonicalFile(),file);
+	       		if(f.isDirectory()) {
+	       			findDbDirs(dbDirs,f,depth+1);
+	       		}
+            } catch (IOException e) {
+            }
+    	}
+   	}
+
 }
